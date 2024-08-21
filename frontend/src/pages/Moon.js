0e1c935842ad4f0c896340ae319c63cfe83b363b
unknown <sajawal amin>
2024-08-21 14:42:55 +0500

    Initial commit

---
+++ frontend/src/pages/Moon.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+
+function Moon() {
+    return (
+        <div className="flex-col justify-center">
+            <Navbar />
+            <Footer />
+        </div>
+    );
+  }
+  
+  export default Moon;
---
+++ frontend/src/pages/Roadmap.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+
+function Roadmap() {
+    return (
+        <div className="flex-col justify-center">
+            <Navbar />
+            <Footer />
+        </div>
+    );
+  }
+  
+  export default Roadmap;
